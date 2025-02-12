bdff6960cae0d7ba9fb8cc67df83584477a9e232
unknown <Nestor321>
2025-02-11 20:49:33 -0500

    creacion del repositorio y un ejemplo

---
+++ projectnoche/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String name = "nestor";
+        int age = 18;
+        boolean mayorEdad = true;
+
+
+
+        System.out.printf("holiii");
+
+        }
+    }
